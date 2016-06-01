12f6d2988ce8d95739f919eef6225d6b3f7ca3fb
Dennis Choi <dennis.choi7@gmail.com>
2016-05-31 23:42:29 -0400

    Master template

---
+++ assets/js/parallax.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $(window).smartload(function(){
+        if (jQuery.browser.mobile === false){
+            var s = skrollr.init({
+                forceHeight: false
+            });
+            s.refresh($('.parallax'));
+        }
+    });
+});
